8f07ad171840460e2e4e02ba38f0d9a2deca29b9
Prasoon Kumar Sharma <prasoonsharmajnv97@gmail.com>
2026-07-15 22:07:54 +0530

    Add methods to generate access and refresh tokens in user model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -55,4 +55,31 @@ userSchema.methods.isPasswordCorrected = async function () {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
 export const User = mongoose.model("User", userSchema);
