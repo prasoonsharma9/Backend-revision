8697ac278faa25590c2f20909d316a0ded34c8b8
Prasoon Kumar Sharma <prasoonsharmajnv97@gmail.com>
2026-07-15 21:18:36 +0530

    Implement password hashing and comparison in user model; add pagination plugin to video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
     video: {
@@ -33,5 +34,6 @@ const videoSchema = new Schema({
 
 }, { timestamps: true });
 
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
